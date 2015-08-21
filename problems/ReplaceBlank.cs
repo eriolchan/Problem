55f2557e659448cb3b7a82c4c32c5570348dcba7
unknown <erichen@ERICHEN-LAPTOP.fareast.corp.microsoft.com>
2015-08-21 22:51:51 +0800

    check in problem of replace blank

---
+++ problems/ReplaceBlank.cs
@@ -0,0 +1,57 @@
+﻿namespace Problems
+{
+    /// <summary>
+    /// Problem: 替换空格
+    /// 请实现一个函数, 把字符串中的每个空格换成“%20”。
+    /// </summary>
+    public class ReplaceBlank
+    {
+        public static void Process(char[] sentence)
+        {
+            if (sentence == null || sentence.Length == 0)
+            {
+                return;
+            }
+
+            int originalLength = 0;
+            int numberOfBlank = 0;
+            int i = 0;
+
+            while (sentence[i] != '\0')
+            {
+                originalLength++;
+
+                if (sentence[i] == ' ')
+                {
+                    numberOfBlank++;
+                }
+
+                i++;
+            }
+
+            int newLength = originalLength + 2 * numberOfBlank;
+            if (newLength > sentence.Length)
+            {
+                return;
+            }
+
+            int indexOfOriginal = originalLength - 1;
+            int indexOfNew = newLength - 1;
+            while (indexOfOriginal >= 0 && indexOfNew > indexOfOriginal)
+            {
+                if (sentence[indexOfOriginal] != ' ')
+                {
+                    sentence[indexOfNew--] = sentence[indexOfOriginal];
+                }
+                else
+                {
+                    sentence[indexOfNew--] = '0';
+                    sentence[indexOfNew--] = '2';
+                    sentence[indexOfNew--] = '%';
+                }
+
+                indexOfOriginal--;
+            }
+        }
+    }
+}
---
+++ tests/ReplaceBlankTest.cs
@@ -0,0 +1,107 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problems;
+
+    [TestClass]
+    public class ReplaceBlankTest
+    {
+        [TestMethod]
+        public void TestBlankInTheMiddle()
+        {
+            char[] sentence = CreateArray("hello world", 20);
+            ReplaceBlank.Process(sentence);
+
+            CollectionAssert.AreEqual(CreateArray("hello%20world", 20), sentence);
+        }
+
+        [TestMethod]
+        public void TestBlankInTheBegin()
+        {
+            char[] sentence = CreateArray(" helloworld", 20);
+            ReplaceBlank.Process(sentence);
+
+            CollectionAssert.AreEqual(CreateArray("%20helloworld", 20), sentence);
+        }
+
+        [TestMethod]
+        public void TestBlankInTheEnd()
+        {
+            char[] sentence = CreateArray("helloworld ", 20);
+            ReplaceBlank.Process(sentence);
+
+            CollectionAssert.AreEqual(CreateArray("helloworld%20", 20), sentence);
+        }
+
+        [TestMethod]
+        public void TestMultipleBlanks()
+        {
+            char[] sentence = CreateArray("hello  world", 20);
+            ReplaceBlank.Process(sentence);
+
+            CollectionAssert.AreEqual(CreateArray("hello%20%20world", 20), sentence);
+        }
+
+        [TestMethod]
+        public void TestNull()
+        {
+            char[] sentence = null;
+            ReplaceBlank.Process(sentence);
+
+            Assert.IsNull(sentence);
+        }
+
+        [TestMethod]
+        public void TestEmpty()
+        {
+            char[] sentence = new char[0];
+            ReplaceBlank.Process(sentence);
+
+            CollectionAssert.AreEqual(new char[0], sentence);
+        }
+
+        [TestMethod]
+        public void TestOnlyOneBlank()
+        {
+            char[] sentence = CreateArray(" ", 20);
+            ReplaceBlank.Process(sentence);
+
+            CollectionAssert.AreEqual(CreateArray("%20", 20), sentence);
+        }
+
+        [TestMethod]
+        public void TestNoBlank()
+        {
+            char[] sentence = CreateArray("helloworld", 20);
+            ReplaceBlank.Process(sentence);
+
+            CollectionAssert.AreEqual(CreateArray("helloworld", 20), sentence);
+        }
+
+        [TestMethod]
+        public void TestAllBlanks()
+        {
+            char[] sentence = CreateArray("   ", 20);
+            ReplaceBlank.Process(sentence);
+
+            CollectionAssert.AreEqual(CreateArray("%20%20%20", 20), sentence);
+        }
+
+        [TestMethod]
+        public void TestInsufficientArrayLength()
+        {
+            char[] sentence = CreateArray("hello  world", 15);
+            ReplaceBlank.Process(sentence);
+
+            CollectionAssert.AreEqual(CreateArray("hello  world", 15), sentence);
+        }
+
+        private char[] CreateArray(string content, int length)
+        {
+            char[] sentence = new char[length];
+            content.ToCharArray().CopyTo(sentence, 0);
+
+            return sentence;
+        }
+    }
+}
