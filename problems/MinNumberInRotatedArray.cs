20dae72a5eb7d3732ce05c229645fbfda76aa21e
Eric Chen <eriolchan@gmail.com>
2015-09-17 21:03:16 +0800

    checkin for problem of find min number in rotated array

---
+++ problems/MinNumberInRotatedArray.cs
@@ -0,0 +1,65 @@
+﻿namespace Problems
+{
+    using System;
+
+    /// <summary>
+    /// Problem: 旋转数组的最小数字
+    /// 把一个数组最开始的若干个元素搬到数组的末尾, 我们称之为
+    /// 数组的旋转。输入一个递增排序的数组的一个旋转, 输出旋转数组
+    /// 的最小元素。
+    /// </summary>
+    public class MinNumberInRotatedArray
+    {
+        public static int Process(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                throw new ArgumentException(Constants.InvalidInput);
+            }
+
+            int start = 0;
+            int end = array.Length - 1;
+            int mid = start;
+
+            while (array[start] >= array[end])
+            {
+                if (end == start + 1)
+                {
+                    mid = end;
+                    break;
+                }
+
+                mid = (start + end) / 2;
+
+                if (array[start] == array[end] && array[mid] == array[start])
+                {
+                    return MinInOrder(array, start, end);
+                }
+
+                if (array[mid] >= array[start])
+                {
+                    start = mid;
+                }
+                else if (array[mid] <= array[end])
+                {
+                    end = mid;
+                }
+            }
+
+            return array[mid];
+        }
+        private static int MinInOrder(int[] array, int start, int end)
+        {
+            int min = array[start];
+            for (int i = start + 1; i <= end; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+    }
+}
---
+++ tests/MinNumberInRotatedArrayTest.cs
@@ -0,0 +1,80 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problems;
+    using System;
+
+    [TestClass]
+    public class MinNumberInRotatedArrayTest
+    {
+        [TestMethod]
+        public void TestNoDuplicatedElement()
+        {
+            int[] array = new int[] { 3, 4, 5, 1, 2 };
+            Assert.AreEqual(1, MinNumberInRotatedArray.Process(array));
+        }
+
+        [TestMethod]
+        public void TestMinIsDuplicatedElement()
+        {
+            int[] array = new int[] { 3, 4, 5, 1, 1, 2 };
+            Assert.AreEqual(1, MinNumberInRotatedArray.Process(array));
+        }
+
+        [TestMethod]
+        public void TestMinIsNotDuplicatedElement()
+        {
+            int[] array = new int[] { 3, 4, 5, 1, 2, 2 };
+            Assert.AreEqual(1, MinNumberInRotatedArray.Process(array));
+        }
+
+        [TestMethod]
+        public void TestStartAndEndSameNotMin()
+        {
+            int[] array = new int[] { 1, 0, 1, 1, 1 };
+            Assert.AreEqual(0, MinNumberInRotatedArray.Process(array));
+        }
+
+        [TestMethod]
+        public void TestMinIsBothStartAndEnd()
+        {
+            int[] array = new int[] { 1, 1, 1, 2, 1 };
+            Assert.AreEqual(1, MinNumberInRotatedArray.Process(array));
+        }
+
+        [TestMethod]
+        public void TestSortedArray()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            Assert.AreEqual(1, MinNumberInRotatedArray.Process(array));
+        }
+
+        [TestMethod]
+        public void TestSameElements()
+        {
+            int[] array = new int[] { 1, 1, 1, 1, 1 };
+            Assert.AreEqual(1, MinNumberInRotatedArray.Process(array));
+        }
+
+        [TestMethod]
+        public void TestOnlyOneElement()
+        {
+            int[] array = new int[] { 1 };
+            Assert.AreEqual(1, MinNumberInRotatedArray.Process(array));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestNull()
+        {
+            MinNumberInRotatedArray.Process(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestEmptyArray()
+        {
+            MinNumberInRotatedArray.Process(new int[0]);
+        }
+    }
+}
