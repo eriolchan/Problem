acd78c82551ca7a1b05dc1ccdb26e421ee266e60
Eric Chen <eriolchan@gmail.com>
2015-10-15 23:15:19 +0800

    checkin for problem of print 1 to max of n digits

---
+++ problems/Print1ToMaxOfNDigits.cs
@@ -0,0 +1,76 @@
+﻿namespace Problems
+{
+    using System.Text;
+
+    /// <summary>
+    /// Problem: 打印1到最大的N位数
+    /// 输入数字n, 按顺序打印出从1到最大的n位十进制数。
+    /// </summary>
+    public class Print1ToMaxOfNDigits
+    {
+        private const int Zero = 48;
+        private static StringBuilder output = new StringBuilder();
+
+        public static string Result
+        {
+            get
+            {
+                return Helper.Output(output);
+            }
+        }
+
+        public static void Process_Recursively(int n)
+        {
+            if (n <= 0)
+            {
+                return;
+            }
+
+            char[] number = new char[n];
+
+            for (int i = 0; i < 10; ++i)
+            {
+                number[0] = (char)(i + Zero);
+                ProcessCore(number, n, 0);
+            }
+        }
+
+        private static void ProcessCore(char[] number, int length, int index)
+        {
+            if (index == length - 1)
+            {
+                PrintNumber(number);
+                return;
+            }
+
+            for (int i = 0; i < 10; ++i)
+            {
+                number[index + 1] = (char)(i + Zero);
+                ProcessCore(number, length, index + 1);
+            }
+        }
+
+        private static void PrintNumber(char[] number)
+        {
+            bool isBeginningZero = true;
+            
+            for (int i = 0; i < number.Length; ++i)
+            {
+                if (isBeginningZero && number[i] != '0')
+                {
+                    isBeginningZero = false;
+                }
+
+                if (!isBeginningZero)
+                {
+                    output.Append(number[i]);
+                }
+            }
+
+            if (!isBeginningZero)
+            {
+                output.Append(',');
+            }
+        }
+    }
+}
---
+++ tests/Print1ToMaxOfNDigitsTest.cs
@@ -0,0 +1,50 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problems;
+    using System.Text;
+
+    [TestClass]
+    public class Print1ToMaxOfNDigitsTest
+    {
+        [TestMethod]
+        public void TestNIsNegative()
+        {
+            Print1ToMaxOfNDigits.Process_Recursively(-1);
+            Assert.IsNull(Print1ToMaxOfNDigits.Result);
+        }
+
+        [TestMethod]
+        public void TestNIsZero()
+        {
+            Print1ToMaxOfNDigits.Process_Recursively(0);
+            Assert.IsNull(Print1ToMaxOfNDigits.Result);
+        }
+
+        [TestMethod]
+        public void TestNIsOne()
+        {
+            StringBuilder expected = new StringBuilder();
+            for (int i = 1; i < 10; ++i)
+            {
+                expected.Append(i + ",");
+            }
+
+            Print1ToMaxOfNDigits.Process_Recursively(1);
+            Assert.AreEqual(expected.ToString(), Print1ToMaxOfNDigits.Result);
+        }
+
+        [TestMethod]
+        public void TestNIsPositive()
+        {
+            StringBuilder expected = new StringBuilder();
+            for (int i = 1; i < 1000; ++i)
+            {
+                expected.Append(i + ",");
+            }
+
+            Print1ToMaxOfNDigits.Process_Recursively(3);
+            Assert.AreEqual(expected.ToString(), Print1ToMaxOfNDigits.Result);
+        }
+    }
+}
