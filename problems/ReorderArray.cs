5364adcec52fa7751ee4e82a888498755e86d7f2
Eric Chen <eriolchan@gmail.com>
2015-10-19 22:12:12 +0800

    checkin for problem of reorder array

---
+++ problems/ReorderArray.cs
@@ -0,0 +1,39 @@
+﻿namespace Problems
+{
+    using System;
+
+    /// <summary>
+    /// Problem: 调整数组顺序使奇数位于偶数前面
+    /// 输入一个整数数组, 实现一个函数来调整该数组中数字的顺序,
+    /// 使得所有奇数位于数组的前半部, 所有偶数位于数组的后半部。
+    /// </summary>
+    public class ReorderArray
+    {
+        public static void Process_OddEven(int[] array)
+        {
+            Process(array, IsOdd);
+        }
+
+        private static bool IsOdd(int i)
+        {
+            return (i & 1) == 1;
+        }
+
+        private static void Process(int[] array, Func<int, bool> function)
+        {
+            if (array == null || array.Length == 0 || function == null)
+            {
+                return;
+            }
+
+            int i = -1;
+            for (int j = 0; j < array.Length; ++j)
+            {
+                if (function(array[j]))
+                {
+                    Helper.Swap(array, ++i, j);
+                }
+            }
+        }
+    }
+}
---
+++ tests/ReorderArrayTest.cs
@@ -0,0 +1,73 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problems;
+
+    [TestClass]
+    public class ReorderArrayTest
+    {
+        [TestMethod]
+        public void TestOddIsBeforeEven()
+        {
+            int[] array = new int[] { 1, 3, 5, 2, 4 };
+            int[] expected = Helper.CreateArray(array, array.Length);
+
+            ReorderArray.Process_OddEven(array);
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod]
+        public void TestOddIsAfterEven()
+        {
+            int[] array = new int[] { 2, 4, 1, 3, 5 };
+            int[] expected = new int[] { 1, 3, 5, 4, 2 };
+
+            ReorderArray.Process_OddEven(array);
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod]
+        public void TestOddCrossEven()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            int[] expected = new int[] { 1, 3, 5, 4, 2 };
+
+            ReorderArray.Process_OddEven(array);
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod]
+        public void TestOnlyOneOddElement()
+        {
+            int[] array = new int[] { 1 };
+            int[] expected = Helper.CreateArray(array, array.Length);
+
+            ReorderArray.Process_OddEven(array);
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod]
+        public void TestOnlyOneEvenElement()
+        {
+            int[] array = new int[] { 2 };
+            int[] expected = Helper.CreateArray(array, array.Length);
+
+            ReorderArray.Process_OddEven(array);
+            CollectionAssert.AreEqual(expected, array);
+        }
+
+        [TestMethod]
+        public void TestNull()
+        {
+            int[] array = null;
+
+            ReorderArray.Process_OddEven(array);
+            Assert.IsNull(array);
+
+            array = new int[0];
+
+            ReorderArray.Process_OddEven(array);
+            CollectionAssert.AreEqual(new int[0], array);
+        }
+    }
+}
