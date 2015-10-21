fddc6d05c856c5967bba9ad6e7c7c24dd8e62b20
Eric Chen <eriolchan@gmail.com>
2015-10-21 23:33:51 +0800

    checkin for problem of nth node from end

---
+++ problems/KthNodeFromEnd.cs
@@ -0,0 +1,40 @@
+﻿namespace Problems
+{
+    /// <summary>
+    /// Problem: 链表中倒数第k个结点
+    /// 输入一个链表, 输出该链表中倒数第k个结点。从1开始计数,
+    /// 即链表的尾结点是倒数第一个结点。
+    /// </summary>
+    public class KthNodeFromEnd
+    {
+        public static ListNode Process(ListNode head, uint k)
+        {
+            if (head == null || k == 0)
+            {
+                return null;
+            }
+
+            ListNode ahead = head;
+            for (int i = 0; i < k - 1; ++i)
+            {
+                if (ahead.Next != null)
+                {
+                    ahead = ahead.Next;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            ListNode behind = head;
+            while (ahead.Next != null)
+            {
+                ahead = ahead.Next;
+                behind = behind.Next;
+            }
+
+            return behind;
+        }
+    }
+}
---
+++ tests/KthNodeFromEndTest.cs
@@ -0,0 +1,58 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problems;
+
+    [TestClass]
+    public class KthNodeFromEndTest
+    {
+        [TestMethod]
+        public void TestNodeInTheMiddle()
+        {
+            ListNode head = Helper.CreateLinkedList(new int[] { 1, 2, 3, 4, 5 });
+            Assert.AreEqual(4, KthNodeFromEnd.Process(head, 2).Value);
+        }
+
+        [TestMethod]
+        public void TestNodeIsHead()
+        {
+            ListNode head = Helper.CreateLinkedList(new int[] { 1, 2, 3, 4, 5 });
+            Assert.AreEqual(1, KthNodeFromEnd.Process(head, 5).Value);
+        }
+
+        [TestMethod]
+        public void TestNodeInTheEnd()
+        {
+            ListNode head = Helper.CreateLinkedList(new int[] { 1, 2, 3, 4, 5 });
+            Assert.AreEqual(5, KthNodeFromEnd.Process(head, 1).Value);
+        }
+
+        [TestMethod]
+        public void TestOnlyOneNode()
+        {
+            ListNode head = new ListNode(1);
+            Assert.AreEqual(1, KthNodeFromEnd.Process(head, 1).Value);
+        }
+
+        [TestMethod]
+        public void TestNull()
+        {
+            ListNode head = null;
+            Assert.IsNull(KthNodeFromEnd.Process(head, 1));
+        }
+
+        [TestMethod]
+        public void TestKIsZero()
+        {
+            ListNode head = Helper.CreateLinkedList(new int[] { 1, 2, 3, 4, 5 });
+            Assert.IsNull(KthNodeFromEnd.Process(head, 0));
+        }
+
+        [TestMethod]
+        public void TestListLengthIsInsufficient()
+        {
+            ListNode head = Helper.CreateLinkedList(new int[] { 1, 2, 3, 4, 5 });
+            Assert.IsNull(KthNodeFromEnd.Process(head, 6));
+        }
+    }
+}
