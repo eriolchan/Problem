1294435a89f39115541e3b9b6105f7af972aa8d3
Eric Chen <eriolchan@gmail.com>
2015-10-24 22:15:43 +0800

    checkin for problem of find middle node of list

---
+++ problems/FindMiddleNodeOfList.cs
@@ -0,0 +1,29 @@
+﻿namespace Problems
+{
+    /// <summary>
+    /// Problem: 链表的中间结点
+    /// 如果链表中结点总数为奇数, 返回中间结点; 如果结点总数是偶数,
+    /// 返回中间两个结点的任意一个。
+    /// </summary>
+    public class FindMiddleNodeOfList
+    {
+        public static ListNode Process(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while (fast.Next != null && fast.Next.Next != null)
+            {
+                fast = fast.Next.Next;
+                slow = slow.Next;
+            }
+
+            return slow;
+        }
+    }
+}
---
+++ tests/FindMiddleNodeOfListTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problems;
+
+    [TestClass]
+    public class FindMiddleNodeOfListTest
+    {
+        [TestMethod]
+        public void TestLengthIsOdd()
+        {
+            ListNode head = Helper.CreateLinkedList(new int[] { 1, 2, 3, 4, 5 });
+            Assert.AreEqual(3, FindMiddleNodeOfList.Process(head).Value);
+        }
+
+        [TestMethod]
+        public void TestLengthIsEven()
+        {
+            ListNode head = Helper.CreateLinkedList(new int[] { 1, 2, 3, 4 });
+            Assert.AreEqual(2, FindMiddleNodeOfList.Process(head).Value);
+        }
+
+        [TestMethod]
+        public void TestOnlyOneNode()
+        {
+            ListNode head = new ListNode(1);
+            Assert.AreEqual(1, FindMiddleNodeOfList.Process(head).Value);
+        }
+
+        [TestMethod]
+        public void TestOnlyTwoNodes()
+        {
+            ListNode head = Helper.CreateLinkedList(new int[] { 1, 2 });
+            Assert.AreEqual(1, FindMiddleNodeOfList.Process(head).Value);
+        }
+
+        [TestMethod]
+        public void TestNull()
+        {
+            ListNode head = null;
+            Assert.IsNull(FindMiddleNodeOfList.Process(head));
+        }
+    }
+}
