c936db9585bb6f21874f204ac20df1b3df72514d
Eric Chen <eriolchan@gmail.com>
2015-10-27 08:49:41 +0800

    rename problem of merge sorted array

---
+++ problems/MergeSortedArray.cs
@@ -0,0 +1,47 @@
+﻿namespace Problems
+{
+    using System;
+
+    /// <summary>
+    /// Problem: 合并两个排序数组
+    /// 有两个排序的数组A1和A2, 内存在A1的末尾有足够多的空余空间
+    /// 容纳A2。请实现一个函数, 把A2中的所有数字插入到A1中并且所有
+    /// 的数字是排序的。
+    /// </summary>
+    public class MergeSortedArray
+    {
+        public static void Process(int[] first, int[] second, int firstCount)
+        {
+            if (first == null || second == null || second.Length == 0)
+            {
+                return;
+            }
+
+            int newLength = firstCount + second.Length;
+            if (newLength > first.Length)
+            {
+                throw new ArgumentException(Constants.InvalidInput);
+            }
+
+            int newIndex = newLength - 1;
+            int firstIndex = firstCount - 1;
+            int secondIndex = second.Length - 1;
+            while (firstIndex >= 0 && secondIndex >= 0)
+            {
+                if (first[firstIndex] >= second[secondIndex])
+                {
+                    first[newIndex--] = first[firstIndex--];
+                }
+                else
+                {
+                    first[newIndex--] = second[secondIndex--];
+                }
+            }
+
+            while (secondIndex >= 0)
+            {
+                first[newIndex--] = second[secondIndex--];
+            }
+        }
+    }
+}
---
+++ tests/MergeSortedArrayTest.cs
@@ -0,0 +1,100 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problems;
+    using System;
+
+    [TestClass]
+    public class MergeSortedArrayTest
+    {
+        [TestMethod]
+        public void TestFirstArrayIsNull()
+        {
+            int[] first = null;
+            int[] second = new int[] { 4, 5, 6 };
+
+            MergeSortedList.Process(first, second, 0);
+            Assert.IsNull(first);
+        }
+
+        [TestMethod]
+        public void TestSecondArrayIsNull()
+        {
+            int[] first = Helper.CreateArray(new int[] { 1, 2, 3 }, 10);
+            int[] second = null;
+
+            MergeSortedList.Process(first, second, 3);
+            CollectionAssert.AreEqual(Helper.CreateArray(new int[] { 1, 2, 3 }, 10), first);
+        }
+
+        [TestMethod]
+        public void TestFirstArrayIsEmpty()
+        {
+            int[] first = new int[10];
+            int[] second = new int[] { 3, 4, 5 };
+
+            MergeSortedList.Process(first, second, 0);
+            CollectionAssert.AreEqual(Helper.CreateArray(second, 10), first);
+        }
+        
+        [TestMethod]
+        public void TestSecondArrayIsEmpty()
+        {
+            int[] first = Helper.CreateArray(new int[] { 1, 2, 3 }, 10);
+            int[] second = new int[0];
+
+            MergeSortedList.Process(first, second, 3);
+            CollectionAssert.AreEqual(Helper.CreateArray(first, 10), first);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestInsufficientArrayLength()
+        {
+            int[] first = Helper.CreateArray(new int[] { 1, 2, 3 }, 5);
+            int[] second = new int[] { 4, 5, 6 };
+
+            MergeSortedList.Process(first, second, 3);
+        }
+
+        [TestMethod]
+        public void TestFirstIsMoreThanSecond()
+        {
+            int[] first = Helper.CreateArray(new int[] { 4, 5, 6 }, 10);
+            int[] second = new int[] { 1, 2, 3 };
+
+            MergeSortedList.Process(first, second, 3);
+            CollectionAssert.AreEqual(Helper.CreateArray(new int[] { 1, 2, 3, 4, 5, 6 }, 10), first);
+        }
+
+        [TestMethod]
+        public void TestFirstIsLessThanSecond()
+        {
+            int[] first = Helper.CreateArray(new int[] { 1, 2, 3 }, 10);
+            int[] second = new int[] { 4, 5, 6 };
+
+            MergeSortedList.Process(first, second, 3);
+            CollectionAssert.AreEqual(Helper.CreateArray(new int[] { 1, 2, 3, 4, 5, 6 }, 10), first);
+        }
+
+        [TestMethod]
+        public void TestFirstInMiddleOfSecond()
+        {
+            int[] first = Helper.CreateArray(new int[] { 2, 4, 5 }, 10);
+            int[] second = new int[] { 1, 3, 6 };
+
+            MergeSortedList.Process(first, second, 3);
+            CollectionAssert.AreEqual(Helper.CreateArray(new int[] { 1, 2, 3, 4, 5, 6 }, 10), first);
+        }
+
+        [TestMethod]
+        public void TestFirstIsSameAsSecond()
+        {
+            int[] first = Helper.CreateArray(new int[] { 1, 2 }, 5);
+            int[] second = new int[] { 1, 2, 3 };
+
+            MergeSortedList.Process(first, second, 2);
+            CollectionAssert.AreEqual(Helper.CreateArray(new int[] { 1, 1, 2, 2, 3 }, 5), first);
+        }
+    }
+}
