e3397e2388fb14282955defbf35cfabc1f38e384
Eric Chen <eriolchan@gmail.com>
2015-11-01 21:47:40 +0800

    checkin for problem of mirror of binary tree

---
+++ problems/MirrorOfBinaryTree.cs
@@ -0,0 +1,59 @@
+﻿namespace Problems
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Problem: 二叉树的镜像
+    /// 请完成一个函数, 输入一个二叉树, 该函数输出它的镜像。
+    /// </summary>
+    public class MirrorOfBinaryTree
+    {
+        public static void Process(TreeNode root)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode current = root;
+            TreeNode temp;
+
+            while (current != null || stack.Count > 0)
+            {
+                while (current != null)
+                {
+                    stack.Push(current);
+
+                    temp = current.Left;
+                    current.Left = current.Right;
+                    current.Right = temp;
+
+                    current = current.Left;
+                }
+
+                if (stack.Count > 0)
+                {
+                    current = stack.Pop().Right;
+                }
+            }
+        }
+
+        public static void ProcessRecursively(TreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            TreeNode temp = root.Left;
+            root.Left = root.Right;
+            root.Right = temp;
+
+            if (root.Left != null)
+            {
+                ProcessRecursively(root.Left);
+            }
+
+            if (root.Right != null)
+            {
+                ProcessRecursively(root.Right);
+            }
+        }
+    }
+}
---
+++ tests/MirrorOfBinaryTreeTest.cs
@@ -0,0 +1,105 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problems;
+
+    [TestClass]
+    public class MirrorOfBinaryTreeTest
+    {
+        [TestMethod]
+        public void TestCompleteTree()
+        {
+            TreeNode root = Helper.CreateTree(TreeType.CompleteTree);
+            TreeNode expected = new TreeNode(1);
+            expected.Left = new TreeNode(3);
+            expected.Left.Left = new TreeNode(7);
+            expected.Left.Right = new TreeNode(6);
+            expected.Right = new TreeNode(2);
+            expected.Right.Left = new TreeNode(5);
+            expected.Right.Right = new TreeNode(4);
+
+            MirrorOfBinaryTree.Process(root);
+            Assert.IsTrue(Helper.CompareBinaryTree(expected, root));
+
+            root = Helper.CreateTree(TreeType.CompleteTree);
+            MirrorOfBinaryTree.ProcessRecursively(root);
+            Assert.IsTrue(Helper.CompareBinaryTree(expected, root));
+        }
+
+        [TestMethod]
+        public void TestNotCompleteTree()
+        {
+            TreeNode root = Helper.CreateTree(TreeType.NotCompleteTree);
+            TreeNode expected = new TreeNode(1);
+            expected.Left = new TreeNode(3);
+            expected.Left.Left = new TreeNode(6);
+            expected.Left.Right = new TreeNode(5);
+            expected.Left.Left.Right = new TreeNode(8);
+            expected.Right = new TreeNode(2);
+            expected.Right.Right = new TreeNode(4);
+            expected.Right.Right.Left = new TreeNode(7);
+
+            MirrorOfBinaryTree.Process(root);
+            Assert.IsTrue(Helper.CompareBinaryTree(expected, root));
+
+            root = Helper.CreateTree(TreeType.NotCompleteTree);
+            MirrorOfBinaryTree.ProcessRecursively(root);
+            Assert.IsTrue(Helper.CompareBinaryTree(expected, root));
+        }
+
+        [TestMethod]
+        public void TestNoLeftChildTree()
+        {
+            TreeNode root = Helper.CreateTree(TreeType.NoLeftChildTree);
+            TreeNode expected = Helper.CreateTree(TreeType.NoRightChildTree);
+
+            MirrorOfBinaryTree.Process(root);
+            Assert.IsTrue(Helper.CompareBinaryTree(expected, root));
+
+            root = Helper.CreateTree(TreeType.NoLeftChildTree);
+            MirrorOfBinaryTree.ProcessRecursively(root);
+            Assert.IsTrue(Helper.CompareBinaryTree(expected, root));
+        }
+
+        [TestMethod]
+        public void TestNoRightChildTree()
+        {
+            TreeNode root = Helper.CreateTree(TreeType.NoRightChildTree);
+            TreeNode expected = Helper.CreateTree(TreeType.NoLeftChildTree);
+
+            MirrorOfBinaryTree.Process(root);
+            Assert.IsTrue(Helper.CompareBinaryTree(expected, root));
+
+            root = Helper.CreateTree(TreeType.NoRightChildTree);
+            MirrorOfBinaryTree.ProcessRecursively(root);
+            Assert.IsTrue(Helper.CompareBinaryTree(expected, root));
+        }
+
+        [TestMethod]
+        public void TestOnlyOneNode()
+        {
+            TreeNode root = new TreeNode(1);
+            TreeNode expected = new TreeNode(1);
+
+            MirrorOfBinaryTree.Process(root);
+            Assert.IsTrue(Helper.CompareBinaryTree(expected, root));
+
+            root = new TreeNode(1);
+            MirrorOfBinaryTree.ProcessRecursively(root);
+            Assert.IsTrue(Helper.CompareBinaryTree(expected, root));
+        }
+
+        [TestMethod]
+        public void TestNull()
+        {
+            TreeNode root = null;
+
+            MirrorOfBinaryTree.Process(root);
+            Assert.IsNull(root);
+
+            root = null;
+            MirrorOfBinaryTree.ProcessRecursively(root);
+            Assert.IsNull(root);
+        }
+    }
+}
