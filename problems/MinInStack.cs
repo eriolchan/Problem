5584e5637529f6079e08a2cf7b48b039d5a0822b
Eric Chen <eriolchan@gmail.com>
2015-11-08 16:50:37 +0800

    checkin for problem of min in stack

---
+++ problems/MinInStack.cs
@@ -0,0 +1,59 @@
+﻿namespace Problems
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Problem: 包含min函数的栈
+    /// 定义栈的数据结构, 请在该类型中实现一个能够得到栈
+    /// 的最小元素的min函数。在该栈中, 调用min, push, pop的
+    /// 时间复杂度为O(1)。
+    /// </summary>
+    public class MinInStack<T> where T : IComparable
+    {
+        private Stack<T> stack;
+        private Stack<T> minStack;
+
+        public MinInStack()
+        {
+            stack = new Stack<T>();
+            minStack = new Stack<T>();
+        }
+
+        public void Push(T value)
+        {
+            stack.Push(value);
+
+            if (minStack.Count == 0)
+            {
+                minStack.Push(value);
+            }
+            else
+            {
+                T min = Min();
+                minStack.Push(value.CompareTo(min) < 0 ? value : min);
+            }
+        }
+
+        public T Pop()
+        {
+            if (stack.Count == 0)
+            {
+                throw new InvalidOperationException(Constants.EmptyContainer);
+            }
+
+            minStack.Pop();
+            return stack.Pop();
+        }
+
+        public T Min()
+        {
+            if (minStack.Count == 0)
+            {
+                throw new InvalidOperationException(Constants.EmptyContainer);
+            }
+
+            return minStack.Peek();
+        }
+    }
+}
---
+++ tests/MinInStackTest.cs
@@ -0,0 +1,121 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problems;
+    using System;
+
+    [TestClass]
+    public class MinInStackTest
+    {
+        [TestMethod]
+        public void TestInsertIncrease()
+        {
+            MinInStack<int> stack = new MinInStack<int>();
+
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Min());
+
+            stack.Push(2);
+            Assert.AreEqual(1, stack.Min());
+
+            stack.Push(3);
+            Assert.AreEqual(1, stack.Min());
+
+            stack.Pop();
+            Assert.AreEqual(1, stack.Min());
+
+            stack.Pop();
+            Assert.AreEqual(1, stack.Min());
+        }
+        
+        [TestMethod]
+        public void TestInsertDecrease()
+        {
+            MinInStack<int> stack = new MinInStack<int>();
+
+            stack.Push(3);
+            Assert.AreEqual(3, stack.Min());
+
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Min());
+
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Min());
+
+            stack.Pop();
+            Assert.AreEqual(2, stack.Min());
+
+            stack.Pop();
+            Assert.AreEqual(3, stack.Min());
+        }
+        
+        [TestMethod]
+        public void TestInsertUnsorted()
+        {
+            MinInStack<int> stack = new MinInStack<int>();
+
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Min());
+
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Min());
+
+            stack.Push(3);
+            Assert.AreEqual(1, stack.Min());
+
+            stack.Pop();
+            Assert.AreEqual(1, stack.Min());
+
+            stack.Pop();
+            Assert.AreEqual(2, stack.Min());
+        }
+
+        [TestMethod]
+        public void TestWithSameElement()
+        {
+            MinInStack<int> stack = new MinInStack<int>();
+
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Min());
+
+            stack.Push(3);
+            Assert.AreEqual(2, stack.Min());
+
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Min());
+
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Min());
+
+            stack.Pop();
+            Assert.AreEqual(2, stack.Min());
+
+            stack.Pop();
+            Assert.AreEqual(2, stack.Min());
+
+            stack.Pop();
+            Assert.AreEqual(2, stack.Min());
+        }
+
+        [TestMethod]
+        public void TestOnlyOneElement()
+        {
+            MinInStack<int> stack = new MinInStack<int>();
+
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Min());
+
+            stack.Pop();
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Min());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestEmpty()
+        {
+            MinInStack<int> stack = new MinInStack<int>();
+            stack.Min();
+        }
+    }
+}
