f2cf99f920f6e73b10490258cc8a42337c3a2408
Eric Chen <eriolchan@gmail.com>
2015-11-17 21:32:54 +0800

    checkin for problem of stack push pop order

---
+++ problems/StackPushPopOrder.cs
@@ -0,0 +1,56 @@
+﻿namespace Problems
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Problem: 栈的压入弹出序列
+    /// 输入两个整数序列, 第一个序列表示栈的压入顺序, 请判断
+    /// 第二个序列是否为该栈的弹出序列。假设压入栈的所有数字
+    /// 均不相等。
+    /// </summary>
+    public class StackPushPopOrder
+    {
+        public static bool Process(int[] pushOrder, int[] popOrder)
+        {
+            bool result = false;
+
+            if (pushOrder != null &&
+                popOrder != null &&
+                pushOrder.Length == popOrder.Length &&
+                pushOrder.Length > 0)
+            {
+                Stack<int> stack = new Stack<int>();
+                int i = 0;
+                int j = 0;
+
+                while (j < popOrder.Length)
+                {
+                    while (stack.Count == 0 || stack.Peek() != popOrder[j])
+                    {
+                        if (i == pushOrder.Length)
+                        {
+                            break;
+                        }
+
+                        stack.Push(pushOrder[i++]);
+                    }
+
+                    if (stack.Peek() != popOrder[j])
+                    {
+                        break;
+                    }
+
+                    stack.Pop();
+                    ++j;
+                }
+
+                if (stack.Count == 0 && j == popOrder.Length)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ tests/StackPushPopOrderTest.cs
@@ -0,0 +1,106 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problems;
+
+    [TestClass]
+    public class StackPushPopOrderTest
+    {
+        [TestMethod]
+        public void TestIsOrder()
+        {
+            int[] pushOrder = new int[] { 1, 2, 3, 4, 5 };
+            int[] popOrder = new int[] { 2, 5, 4, 3, 1 };
+
+            Assert.IsTrue(StackPushPopOrder.Process(pushOrder, popOrder));
+        }
+
+        [TestMethod]
+        public void TestIsOrderWithPopOneByOne()
+        {
+            int[] pushOrder = new int[] { 1, 2, 3, 4, 5 };
+            int[] popOrder = new int[] { 1, 2, 3, 4, 5 };
+
+            Assert.IsTrue(StackPushPopOrder.Process(pushOrder, popOrder));
+        }
+
+        [TestMethod]
+        public void TestIsOrderWithPopAll()
+        {
+            int[] pushOrder = new int[] { 1, 2, 3, 4, 5 };
+            int[] popOrder = new int[] { 5, 4, 3, 2, 1 };
+
+            Assert.IsTrue(StackPushPopOrder.Process(pushOrder, popOrder));
+        }
+
+        [TestMethod]
+        public void TestIsNotOrder()
+        {
+            int[] pushOrder = new int[] { 1, 2, 3, 4, 5 };
+            int[] popOrder = new int[] { 2, 5, 4, 1, 3 };
+
+            Assert.IsFalse(StackPushPopOrder.Process(pushOrder, popOrder));
+        }
+
+        [TestMethod]
+        public void TestHasDifferentElement()
+        {
+            int[] pushOrder = new int[] { 1, 2, 3, 4, 5 };
+            int[] popOrder = new int[] { 3, 5, 4, 0, 1 };
+
+            Assert.IsFalse(StackPushPopOrder.Process(pushOrder, popOrder));
+        }
+
+        [TestMethod]
+        public void TestOnlyOneElement()
+        {
+            int[] pushOrder = new int[] { 1 };
+            int[] popOrder = new int[] { 1 };
+            Assert.IsTrue(StackPushPopOrder.Process(pushOrder, popOrder));
+
+            popOrder = new int[] { 2 };
+            Assert.IsFalse(StackPushPopOrder.Process(pushOrder, popOrder));
+        }
+
+        [TestMethod]
+        public void TestLengthIsNotEqual()
+        {
+            int[] pushOrder = new int[] { 1 };
+            int[] popOrder = new int[] { 1, 2 };
+            Assert.IsFalse(StackPushPopOrder.Process(pushOrder, popOrder));
+
+            pushOrder = new int[] { 1, 2, 3 };
+            Assert.IsFalse(StackPushPopOrder.Process(pushOrder, popOrder));
+        }
+
+        [TestMethod]
+        public void TestNull()
+        {
+            int[] pushOrder = new int[] { 1 };
+            int[] popOrder = null;
+            Assert.IsFalse(StackPushPopOrder.Process(pushOrder, popOrder));
+
+            pushOrder = null;
+            popOrder = new int[] { 1 };
+            Assert.IsFalse(StackPushPopOrder.Process(pushOrder, popOrder));
+
+            popOrder = null;
+            Assert.IsFalse(StackPushPopOrder.Process(pushOrder, popOrder));
+        }
+
+        [TestMethod]
+        public void TestEmptyArray()
+        {
+            int[] pushOrder = new int[] { 1 };
+            int[] popOrder = new int[0];
+            Assert.IsFalse(StackPushPopOrder.Process(pushOrder, popOrder));
+
+            pushOrder = new int[0];
+            popOrder = new int[] { 1 };
+            Assert.IsFalse(StackPushPopOrder.Process(pushOrder, popOrder));
+
+            popOrder = new int[0];
+            Assert.IsFalse(StackPushPopOrder.Process(pushOrder, popOrder));
+        }
+    }
+}
