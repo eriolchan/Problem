bf69f29c0d0d2ff7c6738512b690cd82537bd822
Eric Chen <eriolchan@gmail.com>
2015-11-21 22:57:03 +0800

    checkin for problem of sequence of BST

---
+++ problems/SequenceOfBST.cs
@@ -0,0 +1,48 @@
+﻿namespace Problems
+{
+    /// <summary>
+    /// Problem: 二叉搜索树的后序遍历序列
+    /// 输入一个整数数组, 判断该数组是不是某个二叉搜索树
+    /// 的后序遍历的结果。假设输入的数组的任意两个数字都
+    /// 互不相同。
+    /// </summary>
+    public class SequenceOfBST
+    {
+        public static bool Process(int[] sequence)
+        {
+            if (sequence == null || sequence.Length == 0)
+            {
+                return false;
+            }
+
+            return ProcessCore(sequence, 0, sequence.Length - 1);
+        }
+
+        private static bool ProcessCore(int[] sequence, int start, int end)
+        {
+            if (start >= end)
+            {
+                return true;
+            }
+
+            int root = sequence[end];
+
+            int i = start;
+            while (i < end && sequence[i] < root)
+            {
+                ++i;
+            }
+
+            for (int j = i; j < end; ++j)
+            {
+                if (sequence[j] < root)
+                {
+                    return false;
+                }
+            }
+
+            return ProcessCore(sequence, start, i - 1) &&
+                ProcessCore(sequence, i, end - 1);
+        }
+    }
+}
---
+++ tests/SequenceOfBSTTest.cs
@@ -0,0 +1,65 @@
+﻿namespace Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Problems;
+
+    [TestClass]
+    public class SequenceOfBSTTest
+    {
+        [TestMethod]
+        public void TestIsNotSequence()
+        {
+            int[] sequence = new int[] { 4, 6, 12, 8, 16, 14, 10 };
+            Assert.IsFalse(SequenceOfBST.Process(sequence));
+        }
+
+        [TestMethod]
+        public void TestCompleteTree()
+        {
+            int[] sequence = new int[] { 4, 8, 6, 12, 16, 14, 10 };
+            Assert.IsTrue(SequenceOfBST.Process(sequence));
+        }
+
+        [TestMethod]
+        public void TestNotCompleteTree()
+        {
+            int[] sequence = new int[] { 4, 6, 7, 5 };
+            Assert.IsTrue(SequenceOfBST.Process(sequence));
+        }
+
+        [TestMethod]
+        public void TestNoLeftChildTree()
+        {
+            int[] sequence = new int[] { 5, 4, 3, 2, 1 };
+            Assert.IsTrue(SequenceOfBST.Process(sequence));
+        }
+
+        [TestMethod]
+        public void TestNoRightChildTree()
+        {
+            int[] sequence = new int[] { 1, 2, 3, 4, 5 };
+            Assert.IsTrue(SequenceOfBST.Process(sequence));
+        }
+
+        [TestMethod]
+        public void TestOnlyOneNode()
+        {
+            int[] sequence = new int[] { 1 };
+            Assert.IsTrue(SequenceOfBST.Process(sequence));
+        }
+
+        [TestMethod]
+        public void TestNull()
+        {
+            int[] sequence = null;
+            Assert.IsFalse(SequenceOfBST.Process(sequence));
+        }
+
+        [TestMethod]
+        public void TestEmpty()
+        {
+            int[] sequence = new int[0];
+            Assert.IsFalse(SequenceOfBST.Process(sequence));
+        }
+    }
+}
