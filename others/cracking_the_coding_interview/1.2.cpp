d35e76d2cd929e540f4846e4936ca4f21b8f8a16
Eric Chen <erichen@microsoft.com>
2016-04-11 15:54:53 +0800

    checkin 1.2.cpp

---
+++ others/cracking_the_coding_interview/1.2.cpp
@@ -0,0 +1,52 @@
+// Problem 1.2
+// Write code to reverse a C-Style String.
+// (C-String means that "abcd" is presented as five characters, including the null character.)
+
+#include<iostream>
+
+using namespace std;
+
+void swap(char &a, char &b)
+{
+  a = a ^ b;
+  b = a ^ b;
+  a = a ^ b;
+}
+
+void reverse(char *s)
+{
+  if (!s)
+  {
+    return;
+  }
+
+  char *p = s, *q = s;
+  while (*q)
+  {
+    ++q;
+  }
+  --q;
+
+  while (p < q)
+  {
+    swap(*p++, *q--);
+  }
+}
+
+void test(char *actual, char *expected)
+{
+  cout << "actual: " << actual << ", expected: " << expected << endl;
+}
+
+int main()
+{
+  char *s = NULL;
+  reverse(s);
+  cout << "actual: " << (s ? s : "NULL") << ", expected: NULL" << endl;
+
+  char str[] = "0123456789";
+  reverse(str);
+  test(str, "9876543210");
+
+  return 0;
+}
