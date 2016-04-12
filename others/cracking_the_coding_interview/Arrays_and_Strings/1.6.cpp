75dcca34308825ce9bec9e6fc892e9a6659b6130
Eric Chen <eriolchan@gmail.com>
2016-04-12 23:47:03 +0800

    checkin 1.6.cpp

---
+++ others/cracking_the_coding_interview/Arrays_and_Strings/1.6.cpp
@@ -0,0 +1,69 @@
+// Problem 1.6
+// Given an image represented by an N*N matrix, where each pixel in the image is 4 bytes,
+// write a method to rotate the image by 90 degrees.
+// Can you do this in place?
+
+#include<iostream>
+
+using namespace std;
+
+void swap(int &a, int &b)
+{
+  a = a ^ b;
+  b = a ^ b;
+  a = a ^ b;
+}
+
+void rotate(int **matrix, int n)
+{
+  for (int i = 0; i < n; ++i)
+  {
+    for (int j = i + 1; j < n; ++j)
+    {
+      swap(*((int *)matrix + n * i + j),
+           *((int *)matrix + n * j + i));
+    }
+  }
+
+  for (int i = 0; i < n / 2; ++i)
+  {
+    for (int j = 0; j < n; ++j)
+    {
+      swap(*((int *)matrix + n * i + j),
+           *((int *)matrix + n * (n - 1 - i) + j));
+    }
+  }
+}
+
+void print(int **matrix, int n)
+{
+  for (int i = 0; i < n; ++i)
+  {
+    for (int j = 0; j < n; ++j)
+    {
+      cout << *((int *)matrix + n * i + j) << ' ';
+    }
+
+    cout << endl;
+  }
+}
+
+int main()
+{
+  int matrix[4][4] = {
+     1,  2,  3,  4,
+     5,  6,  7,  8, 
+     9, 10, 11, 12,
+    13, 14, 15, 16
+  };
+
+  cout << "Original matrix:" << endl;
+  print((int **)matrix, 4);
+
+  rotate((int **)matrix, 4);
+
+  cout << "\nAfter rotate:" << endl;
+  print((int **)matrix, 4);
+
+  return 0;
+}
