9cbb9535b152eaa538e97fb81d8b96554456bde8
Eric Chen <erichen@microsoft.com>
2016-07-08 11:27:09 +0800

    checkin code of question 2.5

--- others/CrackingTheCodingInterview/src/LinkedLists/Question_2_5.java
+++ others/CrackingTheCodingInterview/src/LinkedLists/Question_2_5.java
@@ -5,11 +5,12 @@ package LinkedLists;
  * You have two numbers represented by a linked list, where each node contains a single digit.
  * The digits are stored in reverse order, such that the 1's digit is at the head of the list.
  * Write a function that adds the two numbers and returns the sum as a linked list.
+ * Suppose the digits are stored in forward order. Repeat the above problem.
  */
 public class Question_2_5 {
 
     // O(n)
-    public static ListNode add(ListNode n1, ListNode n2) {
+    public static ListNode addListsInReverseOrder(ListNode n1, ListNode n2) {
         int carry = 0;
         ListNode head = null;
         ListNode previous = null;
@@ -39,5 +40,77 @@ public class Question_2_5 {
         }
         
         return head;
+    }
+    
+    // O(n)
+    public static ListNode addListsInOrder(ListNode n1, ListNode n2) {
+        if (n1 == null && n2 == null) {
+            return null;
+        }
+        
+        int len1 = getLength(n1);
+        int len2 = getLength(n2);
+        
+        if (len1 < len2) {
+            n1 = padLeft(n1, len2 - len1);
+        } else {
+            n2 = padLeft(n2, len1 - len2);
+        }
+        
+        PartialSum sum = addListsInOrderCore(n1, n2);
+        if (sum.carry != 0) {
+            sum.sum = insertBefore(sum.sum, sum.carry);
+        }
+        
+        return sum.sum;
+    }
+    
+    private static PartialSum addListsInOrderCore(ListNode n1, ListNode n2) {
+        if (n1 == null && n2 == null) {
+            return PartialSum.getInstance();
+        }
+        
+        PartialSum sum = addListsInOrderCore(n1.getNext(), n2.getNext());
+        int value = n1.getData() + n2.getData() + sum.carry;
+        sum.sum = insertBefore(sum.sum, value % 10);
+        sum.carry = value / 10;
+        
+        return sum;
+    }
+    
+    private static int getLength(ListNode n) {
+        int len = 0;
+        while (n != null) {
+            ++len;
+            n = n.getNext();
+        }
+        
+        return len;
+    }
+    
+    private static ListNode padLeft(ListNode head, int length) {
+        for (int i = 0; i < length; ++i) {
+            head = insertBefore(head, 0);
+        }
+        
+        return head;
+    }
+    
+    private static ListNode insertBefore(ListNode head, int data) {
+        ListNode n = new ListNode(data);
+        n.setNext(head);
+        
+        return n;
+    }
+    
+    private static class PartialSum {
+        ListNode sum = null;
+        int carry = 0;
+        
+        private PartialSum() {}
+        
+        public static PartialSum getInstance() {
+            return new PartialSum();
+        }
     }
 }
--- others/CrackingTheCodingInterview/test/LinkedLists/Question_2_5Test.java
+++ others/CrackingTheCodingInterview/test/LinkedLists/Question_2_5Test.java
@@ -7,42 +7,58 @@ import org.junit.Test;
 public class Question_2_5Test {
 
     @Test
-    public void testAdd_SameLength() {
+    public void testAddLists_SameLength() {
         ListNode n1 = ListNodeTest.createLinkedList(new int[] { 3, 1, 5 });
         ListNode n2 = ListNodeTest.createLinkedList(new int[] { 5, 9, 2 });
         
         ListNode expected = ListNodeTest.createLinkedList(new int[] { 8, 0, 8});
-        ListNode actual = Question_2_5.add(n1, n2);
+        ListNode actual = Question_2_5.addListsInReverseOrder(n1, n2);
+        ListNodeTest.assertLinkedListEquals(expected, actual);
+        
+        expected = ListNodeTest.createLinkedList(new int[] { 9, 0, 7 });
+        actual = Question_2_5.addListsInOrder(n1, n2);
         ListNodeTest.assertLinkedListEquals(expected, actual);
     }
     
     @Test
-    public void testAdd_DifferentLength() {
+    public void testAddLists_DifferentLength() {
         ListNode n1 = ListNodeTest.createLinkedList(new int[] { 2, 1 });
         ListNode n2 = ListNodeTest.createLinkedList(new int[] { 3, 2, 1 });
         
         ListNode expected = ListNodeTest.createLinkedList(new int[] { 5, 3, 1});
-        ListNode actual = Question_2_5.add(n1, n2);
+        ListNode actual = Question_2_5.addListsInReverseOrder(n1, n2);
+        ListNodeTest.assertLinkedListEquals(expected, actual);
+        
+        expected = ListNodeTest.createLinkedList(new int[] { 3, 4, 2 });
+        actual = Question_2_5.addListsInOrder(n1, n2);
         ListNodeTest.assertLinkedListEquals(expected, actual);
     }
     
     @Test
-    public void testAdd_ExtraCounter() {
+    public void testAddLists_ExtraCarry() {
         ListNode n1 = new ListNode(1);
         ListNode n2 = ListNodeTest.createLinkedList(new int[] { 9, 9 });
         
         ListNode expected = ListNodeTest.createLinkedList(new int[] { 0, 0, 1});
-        ListNode actual = Question_2_5.add(n1, n2);
+        ListNode actual = Question_2_5.addListsInReverseOrder(n1, n2);
+        ListNodeTest.assertLinkedListEquals(expected, actual);
+        
+        expected = ListNodeTest.createLinkedList(new int[] { 1, 0, 0 });
+        actual = Question_2_5.addListsInOrder(n1, n2);
         ListNodeTest.assertLinkedListEquals(expected, actual);
     }
     
     @Test
-    public void testAdd_OneElement() {
+    public void testAddLists_OneElement() {
         ListNode n1 = new ListNode(1);
         ListNode n2 = new ListNode(1);
         
         ListNode expected = new ListNode(2);
-        ListNode actual = Question_2_5.add(n1, n2);
+        ListNode actual = Question_2_5.addListsInReverseOrder(n1, n2);
+        ListNodeTest.assertLinkedListEquals(expected, actual);
+        
+        expected = new ListNode(2);
+        actual = Question_2_5.addListsInOrder(n1, n2);
         ListNodeTest.assertLinkedListEquals(expected, actual);
     }
     
@@ -51,11 +67,17 @@ public class Question_2_5Test {
         ListNode n1 = null;
         ListNode n2 = ListNodeTest.createLinkedList(new int[] { 1, 2, 3 });
         
-        ListNode actual = Question_2_5.add(n1, n2);
+        ListNode actual = Question_2_5.addListsInReverseOrder(n1, n2);
+        ListNodeTest.assertLinkedListEquals(n2, actual);
+        
+        actual = Question_2_5.addListsInOrder(n1, n2);
         ListNodeTest.assertLinkedListEquals(n2, actual);
         
         n2 = null;
-        actual = Question_2_5.add(n1, n2);
+        actual = Question_2_5.addListsInReverseOrder(n1, n2);
+        assertNull(actual);
+        
+        actual = Question_2_5.addListsInOrder(n1, n2);
         assertNull(actual);
     }
 }
