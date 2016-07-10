4d53596c372a856b65ed77b8469eb289052d8df2
Eric Chen <eriolchan@gmail.com>
2016-07-10 21:53:43 +0800

    checkin draft of question 3.3

---
+++ others/CrackingTheCodingInterview/src/StacksAndQueues/Question_3_3.java
@@ -0,0 +1,13 @@
+package StacksAndQueues;
+
+/**
+ * Implement a data structure SetOfStacks which is composed of several stacks and
+ * should create a new stack once the previous one exceeds capacity.
+ * push() and pop() should behavior identically to a single stack.
+ * FOLLOW UP
+ * Implement a function popAt(int index) which performs a pop operation on
+ * a specific sub stack.
+ */
+public class Question_3_3 {
+
+}
