4694bd59f642dfcd68a96ef304f267977fe915c0
Eric Chen <eriolchan@gmail.com>
2016-08-20 23:02:48 +0800

    checkin draft of question 8.2

---
+++ others/CrackingTheCodingInterview/src/RecursionAndDynamicProgramming/Question_8_2.java
@@ -0,0 +1,13 @@
+package RecursionAndDynamicProgramming;
+
+/**
+ * Question 8.2
+ * Imagine a robot sitting on the upper left corner of grid with r rows
+ * and c columns. the robot can only move in two directions, right and down,
+ * but certain cells are "off limits" such that the robot cannot step on
+ * them. Design an algorithm to find a path for the robot from the top left
+ * to the bottom right.
+ */
+public class Question_8_2 {
+
+}
